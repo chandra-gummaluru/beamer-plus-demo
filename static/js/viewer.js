d0c19ae6d6fe7ce0362881f4cd60d69f5da7e49b
Hasan Malik <hasanimranmalik245@gmail.com>
2026-01-21 21:18:58 -0500

    the viewer can now toggle on and off the screenshare

--- static/js/viewer.js
+++ static/js/viewer.js
@@ -1,12 +1,38 @@
-import { Button } from "./button.js";
+
 
 window.addEventListener("DOMContentLoaded", () => {
 
-const switchContainer = document.getElementById("switch-container");
+    // // when this is true, you show the viewer-controlled presentation.
+    // // when this is false, you show the screen-shared presentation.
+    // // important: if isScreenSharing == false then switchToPresentation must be false.
+    // let switchToPresentation = false;
+    // const viewerContainer = document.getElementById('viewer-container');
+    // const switchPresentationContainer = document.getElementById('switch-presentation-container');
+    // const performSwitch = () => {
+    //   console.log(`screensharing: HAHA ${isScreenSharing}`);
+    //   if (!isScreenSharing) {
+    //     return; // do nothing if it's not screensharing.
+    //   }
 
-const switchBtn = new Button(switchContainer, {
-    label: '<i class="fa-solid fa-display"></i>',
-    className: 'btn'
-});
+    //   if (switchToPresentation) {
+    //     switchPresentationContainer.classList.remove('hidden');
+    //     viewerContainer.classList.add('hidden');
+    //   } else {
+    //     switchPresentationContainer.classList.add('hidden');
+    //     viewerContainer.classList.remove('hidden');
+    //   }
+    // }
+
+    // const switchContainer = document.getElementById("switch-container");
+
+    // const switchBtn = new Button(switchContainer, {
+    //     label: '<i class="fa-solid fa-display"></i>',
+    //     className: 'btn',
+    //     onClick: () => {
+    //         switchToPresentation = !switchToPresentation;
+    //         performSwitch();
+    //         console.log(`Performed switch. Currently switchToPresentation: ${switchToPresentation}`);
+    //     }
+    // });
 
 });
